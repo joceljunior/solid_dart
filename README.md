ff2b52f3d7456c799615e857cc2d5cdd8e2e578f
Jocel <jocel.junior.silva@gmail.com>
2021-11-05 23:11:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Dart - SOLID
+
+Aplicação em Dart para estudar os principios do SOLID
+
+## Descrição
+
+
+
+## Desenvolvimento 
+- [x] Single Responsiblity Principle.
+
+
+- [ ]
+
+
+
+
+## Recursos
+- Flutter_modular
+- Clean Architecture
+- Firebase
+- splash_native
+- Validatorless (Validação formulários)
+  
+
+## Considerações
+- ...
+  
+
---
+++ bin/SingleResponsiblityPrinciple.dart
@@ -0,0 +1,34 @@
+void main(List<String> arguments) {
+  print('Estudando Solid!');
+}
+
+// exemplo do principio responsabilidade unica ferido
+
+class ContaCorrente {
+  validarConta() {
+    // logica que valida a existencia da conta
+  }
+
+  salvarModificacoes() {
+    // logica que edita informações de uma conta
+  }
+}
+
+// Single Responsiblity Principle fala sobre uma classe fazer apenas aquilo que realmente deve fazer - um funcionario nao deve saber excluir um produto,
+//ele deve apenas ter o dominio dos seus dados
+
+class ContaCorrenteRepository {
+  void salvarModificacoes() => print('salvando modificaçoes');
+}
+
+class ContaCorrente2 {
+  final ContaCorrenteRepository _repository = ContaCorrenteRepository();
+  validarConta() {
+    // logica que valida a existencia da conta
+  }
+
+  // minha conta corrente, nao sabe salvar no banco, mas sabe que existe um repositorio para isso
+  void salvarModificacoes() {
+    _repository.salvarModificacoes();
+  }
+}
